02cf9a953483346045baba1a966ce19720b018e5
ER562 <xeryk96@gmail.com>
2026-08-25 18:07:48 +0200

    added odd_even_RBWT

--- odd_even_BWT/odd_even_BWT_tb.sv
+++ odd_even_BWT/odd_even_BWT_tb.sv
@@ -2,8 +2,9 @@
 
 module odd_even_BWT_tb #(
     parameter DATA_WIDTH = 8,
-    parameter DATA_VOLUME = 10,
-    parameter REAL_INPUT_LENGTH = 6
+    parameter DATA_VOLUME = 20,
+    parameter INPUT_ARRAY_LENGTH = 20,   //Length of arrays holding test words. Must be equal or logner than longest input word
+    parameter NUMBER_OF_TEST_WORDS = 7  //Exact number of test words
     )(
 
     );
@@ -39,11 +40,16 @@ module odd_even_BWT_tb #(
         .done (done)
     );
     
-    int input_data [REAL_INPUT_LENGTH - 1 : 0] = '{"b", "a", "n", "a", "n", "a"};
-    int output_data [REAL_INPUT_LENGTH - 1 : 0];
-    int expected_data [REAL_INPUT_LENGTH - 1 : 0] = '{"n", "n", "b", "a", "a", "a"};
-    int expected_original_line_idx = 2;
+    string input_data [0 : NUMBER_OF_TEST_WORDS - 1];
+    int input_data_length [0 : NUMBER_OF_TEST_WORDS - 1];
+    string expected_data [0 : NUMBER_OF_TEST_WORDS - 1];
+    int expected_original_line_idx [0 : NUMBER_OF_TEST_WORDS - 1];
+    
+    int output_bytes [0 : INPUT_ARRAY_LENGTH - 1]; 
+    string output_data;
     int original_line_idx;
+    
+    int test_passed = 0;
     
     realtime start_time;
     realtime end_time;
@@ -52,6 +58,44 @@ module odd_even_BWT_tb #(
     always #5 clk = ~clk;
     
     initial begin
+    
+        //writing test data to arrays
+        input_data[0] = "banana";
+        input_data_length[0] = 6;
+        expected_data[0] = "nnbaaa";
+        expected_original_line_idx[0] = 3;
+        
+        input_data[1] = "mississippi";
+        input_data_length[1] = 11;
+        expected_data[1] = "pssmipissii";
+        expected_original_line_idx[1] = 4;
+        
+        input_data[2] = "abracadabra";
+        input_data_length[2] = 11;
+        expected_data[2] = "rdarcaaaabb";
+        expected_original_line_idx[2] = 2;
+        
+        input_data[3] = "abcdef";
+        input_data_length[3] = 6;
+        expected_data[3] = "fabcde";
+        expected_original_line_idx[3] = 0;
+        
+        input_data[4] = "kajak";
+        input_data_length[4] = 5;
+        expected_data[4] = "kjaka";
+        expected_original_line_idx[4] = 3;
+        
+        input_data[5] = "aaaa";
+        input_data_length[5] = 4;
+        expected_data[5] = "aaaa";
+        expected_original_line_idx[5] = 0;
+        
+        input_data[6] = "ab";
+        input_data_length[6] = 2;
+        expected_data[6] = "ba";
+        expected_original_line_idx[6] = 0;
+    
+        //starting tests
         rst <= 0;
         write_en <= 0;
         read_en <= 0;
@@ -64,49 +108,67 @@ module odd_even_BWT_tb #(
         rst <= 0;
         @(posedge clk);
         
-        //starting timer
-        start_time = $realtime;
-        
-        //writing to module
-        write_en <= 1;
-        for(int i = 0 ; i < REAL_INPUT_LENGTH ; i = i + 1)begin
-            write_data <= input_data[i];
-            @(posedge clk);
-        end
-        write_en <= 0;
-        
-        //starting algorithm
-        be <= 1;
-        @(posedge clk);
-        be <= 0;
-        @(posedge clk);
-        
-        //waiting for algorithm completition and reading data
-        wait(done);
-        
-        read_en <= 1;
-        @(posedge clk);
-        @(posedge clk);
-        for(int i = 0 ; i < REAL_INPUT_LENGTH ; i = i + 1)begin
-            output_data[i] <= read_data;
-            if(original_line)begin
-                original_line_idx <= i;
+        //testing loop
+        for(int z = 0 ; z < NUMBER_OF_TEST_WORDS ; z = z + 1)begin
+            start_time = $realtime;
+            output_data = "";
+            
+            //writing to module
+            write_en <= 1;
+            for(int i = 0 ; i < input_data_length[z] ; i = i + 1)begin
+                write_data <= input_data[z][i];
+                @(posedge clk);
             end
+            write_en <= 0;
+            
+            //starting algorithm
+            be <= 1;
             @(posedge clk);
+            be <= 0;
+            @(posedge clk);
+            
+            //waiting for algorithm completition and reading data
+            wait(done);
+            read_en <= 1;
+            @(posedge clk);
+            @(posedge clk);
+            for(int i = 0 ; i < input_data_length[z] ; i = i + 1)begin
+                output_bytes[i] = read_data;
+                if(original_line)begin
+                    original_line_idx <= i;
+                end
+                @(posedge clk);
+            end
+            read_en <= 0;
+            @(posedge clk);
+            
+            end_time = $realtime;
+            
+            //presenting data
+            output_data = "";
+            for(int i = 0; i < input_data_length[z]; i++) begin
+                output_data = {output_data, string'(output_bytes[i])};
+            end
+            $display("\n------------------------------------------------------");
+            $display("Test: %0d   word: %0s", z, input_data[z]);
+            $display("Algorithm time: %0t", end_time - start_time);
+            for(int i = 0 ; i < input_data_length[z] ; i = i + 1)begin
+                $display("idx: %0d   input: %0s   output: %0s   expected: %0s", i, input_data[z][i], output_data[i], expected_data[z][i]);
+            end
+            $display("Original line idx: %0d   Expected original line idx: %0d", original_line_idx, expected_original_line_idx[z]);
+            if(output_data == expected_data[z] && original_line_idx == expected_original_line_idx[z])begin
+                $display("Test PASSED");
+                test_passed++;
+            end else begin
+                $display("Test FAILED");
+            end
+            $display("------------------------------------------------------");
         end
         
-        end_time = $realtime;
-        #10 @(posedge clk);
-        //presenting tata
-        $display("Algorithm time: %0t", end_time - start_time);
-        for(int i = 0 ; i < REAL_INPUT_LENGTH ; i = i + 1)begin
-            $display("idx: %0d   input: %0s   output: %0s   expected: %0s", i, input_data[i], output_data[i], expected_data[i]);
-        end
-        $display("Original line idx: %0d   Expected original line idx: %0d", original_line_idx, expected_original_line_idx);
-        if(output_data == expected_data && original_line_idx == expected_original_line_idx)begin
-            $display("Algorithm works");
-        end
-        
+        $display("\n------------------------------------------------------");
+        $display("All tests completed");
+        $display("%0d/%0d tests passed", test_passed, NUMBER_OF_TEST_WORDS);
+        $display("------------------------------------------------------");
         $finish;
     end
     
--- odd_even_RBWT/odd_even_RBWT.sv
+++ odd_even_RBWT/odd_even_RBWT.sv
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module odd_even_BWT#(
+module odd_even_RBWT #(
     parameter DATA_WIDTH = 8,
     parameter DATA_VOLUME = 10
     )(
@@ -9,10 +9,10 @@ module odd_even_BWT#(
     
     input logic write_en,
     input logic [DATA_WIDTH - 1 : 0] write_data,
+    input logic original_line,
     
     input logic read_en,
     output logic [DATA_WIDTH - 1 : 0] read_data,
-    output logic original_line,
     
     input logic be,
     output logic done
@@ -20,22 +20,21 @@ module odd_even_BWT#(
     
     //memory
     logic [DATA_WIDTH - 1 : 0] data_memory [DATA_VOLUME - 1 : 0];   //memory that stores data inserted by user
-    logic [$clog2(DATA_VOLUME) - 1 : 0] move_memory [DATA_VOLUME - 1 : 0];   //memory that stores cyclic shifts
+    logic [$clog2(DATA_VOLUME) - 1 : 0] index_memory [DATA_VOLUME - 1 : 0];   //memory that stores indexes of input data
     
     logic [$clog2(DATA_VOLUME + 1) - 1 : 0] memory_pointer; //this always points to place where new element would be stored
+    
+    logic [$clog2(DATA_VOLUME) - 1 : 0] next_line_idx;  //at the start it is original line index than index of next letter to read
     
     //additional variables
     logic [1 : 0] state;    //state of module:
     //0 <- normal work (writing data and starting algorithm)
-    //1 <- sorting between letters at specific index
-    //2 <- sorting between words
-    //3 <- reading only
+    //1 <- sorting
+    //2 <- reading only
     
     //sorting
     logic odd_even; //state of sorting algorithm
-    logic [$clog2(DATA_VOLUME + 1) - 1 : 0] loop_counter;   //loop counter for comparing entire input word (letter by letter)
     logic [DATA_VOLUME - 1 : 1] done_comparing_bus; //bus used for ending comparing loop if all comparators raise 1
-    logic [DATA_VOLUME - 1 : 1] separator_bus;  //prevents sorting of already sorted parts of the word
     
     //creating comparators
     genvar z;
@@ -47,9 +46,11 @@ module odd_even_BWT#(
                 if(state == 1)begin    //normal sorting
                     if(odd_even == 0 && z % 2 != 0 || odd_even && z % 2 == 0)begin
                         if(z < memory_pointer)begin //only comparing data that is acually in register
-                            if(data_memory[(move_memory[z - 1] + loop_counter) % DATA_VOLUME] > data_memory[(move_memory[z] + loop_counter) % DATA_VOLUME] && separator_bus[z] == 0)begin
-                                move_memory[z - 1] <= move_memory[z];
-                                move_memory[z] <= move_memory[z - 1];
+                            if(data_memory[z - 1] < data_memory[z])begin
+                                data_memory[z - 1] <= data_memory[z];
+                                data_memory[z] <= data_memory[z - 1];
+                                index_memory[z - 1] <= index_memory[z];
+                                index_memory[z] <= index_memory[z - 1];
                                 done_comparing_bus[z] <= 0;
                             end else begin  //setting done flag if swap didn't occur
                                 done_comparing_bus[z] <= 1;
@@ -57,10 +58,6 @@ module odd_even_BWT#(
                         end else begin   //setting done flag if there is no data to compare in this comparator
                             done_comparing_bus[z] <= 1;
                         end
-                    end
-                end else if(state == 2)begin    //separating letteres into itso own groups
-                    if(data_memory[move_memory[z - 1] + loop_counter] != data_memory[move_memory[z] + loop_counter])begin
-                        separator_bus[z] <= 1;
                     end
                 end
             end
@@ -73,55 +70,37 @@ module odd_even_BWT#(
             memory_pointer <= 0;
             state <= 0;
             odd_even <= 0;
-            original_line <= 0;
         end else begin
             
             if(state == 0)begin //normal work
                 if(write_en && memory_pointer != DATA_VOLUME)begin
                     data_memory[memory_pointer] <= write_data;
+                    index_memory[memory_pointer] <= memory_pointer;
                     memory_pointer <= memory_pointer + 1;
+                    if(original_line)begin
+                        next_line_idx <= memory_pointer;
+                    end
                 end else if(be)begin
                     state <= 1;
-                    odd_even <= 0;
-                    loop_counter <= 0;
-                    for(int i = 0 ; i < memory_pointer ; i = i + 1)begin    //creating cyclic shifts
-                        move_memory[i] <= i;
-                    end
+                    odd_even <= 0;;
                     done_comparing_bus <= 0;
-                    separator_bus <= 0;
                 end
             end else
             
-            if(state == 1)begin //sorting between letters at specific index
+            if(state == 1)begin //sorting
                 if(&done_comparing_bus)begin
                     state <= 2;
+                    done <= 1;
                 end
                 odd_even <= ~ odd_even;
             end else
             
-            if(state == 2)begin //sorting between words
-                if(loop_counter < memory_pointer)begin  //sorting of next letter
-                    loop_counter <= loop_counter + 1;
-                    state <= 1;
-                    done_comparing_bus <= 0;
-                    odd_even <= 0;
-                end else begin  //sorting ended
-                    state <= 3;
-                    done <= 1;
-                    original_line <= 0;
-                end
-            end else
-            
-            if(state == 3)begin //reading data only
+            if(state == 2)begin //reading data only
                 if(memory_pointer == 0)begin    //no more data to read. returns to state 0
                     state <= 0;
                 end else if(read_en)begin
-                    read_data <= data_memory[move_memory[memory_pointer - 1]];
-                    if(move_memory[memory_pointer - 1] == 0)begin
-                        original_line <= 1;
-                    end else begin
-                        original_line <= 0;
-                    end
+                    read_data <= data_memory[next_line_idx];
+                    next_line_idx <= index_memory[next_line_idx];
                     memory_pointer <= memory_pointer - 1;
                 end
                 done <= 0;
@@ -129,5 +108,6 @@ module odd_even_BWT#(
             
         end
     end
+
     
 endmodule
